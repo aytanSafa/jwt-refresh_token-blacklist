e16b8f73086eb065b907c5656154643f0b7eadc2
safaa <aytansafa@gmail.com>
2023-03-05 00:33:46 +0300

    User Repository Created

--- src/main/java/com/secure/jwttoken/entity/User.java
+++ src/main/java/com/secure/jwttoken/entity/User.java
@@ -3,6 +3,7 @@ package com.secure.jwttoken.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
