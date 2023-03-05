f0ac1b7738de5b8e37888e793df3dd176801d18f
safaa <aytansafa@gmail.com>
2023-03-05 21:12:28 +0300

    Role save to db when application start by JwtTokenApplication.class

--- src/main/java/com/secure/jwttoken/JwttokenApplication.java
+++ src/main/java/com/secure/jwttoken/JwttokenApplication.java
@@ -1,13 +1,31 @@
 package com.secure.jwttoken;
 
+import com.secure.jwttoken.constant.ERole;
+import com.secure.jwttoken.entity.Role;
+import com.secure.jwttoken.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwttokenApplication {
+@RequiredArgsConstructor
+public class JwttokenApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JwttokenApplication.class, args);
     }
 
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Role role = new Role();
+        role.setName(ERole.USER);
+        Role role2 = new Role();
+        role2.setName(ERole.ADMIN);
+        roleRepository.save(role);
+        roleRepository.save(role2);
+    }
 }
