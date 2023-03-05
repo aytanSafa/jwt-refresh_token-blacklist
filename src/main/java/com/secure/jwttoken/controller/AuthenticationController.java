19d7e869d59ce0efe424d71a7c5513d3b196e780
safaa <aytansafa@gmail.com>
2023-03-06 01:16:51 +0300

    getRefreshToken method added in refreshTokenService and login and refresh token endpoint created in Controller.

--- src/main/java/com/secure/jwttoken/controller/AuthenticationController.java
+++ src/main/java/com/secure/jwttoken/controller/AuthenticationController.java
@@ -1,7 +1,11 @@
 package com.secure.jwttoken.controller;
 
 
+import com.secure.jwttoken.payload.request.LoginRequest;
+import com.secure.jwttoken.payload.request.RefreshTokenRequest;
 import com.secure.jwttoken.payload.request.RegisterRequest;
+import com.secure.jwttoken.service.LoginService;
+import com.secure.jwttoken.service.RefreshTokenService;
 import com.secure.jwttoken.service.RegisterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final RegisterService registerService;
-
+    private final LoginService loginService;
+    private final RefreshTokenService refreshTokenService;
     @PostMapping(value = "/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(registerService.saveNewUser(request));
+    }
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> generateToken(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(loginService.authenticate(loginRequest));
+    }
+
+    @PostMapping(value = "/refresh")
+    public ResponseEntity<?> generateRefreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest){
+        return ResponseEntity.ok(refreshTokenService.getRefreshTokenResponse(refreshTokenRequest));
     }
 
 
--- src/main/java/com/secure/jwttoken/service/RefreshTokenService.java
+++ src/main/java/com/secure/jwttoken/service/RefreshTokenService.java
@@ -2,8 +2,11 @@ package com.secure.jwttoken.service;
 
 import com.secure.jwttoken.entity.RefreshToken;
 import com.secure.jwttoken.exceptions.RefreshTokenException;
+import com.secure.jwttoken.payload.request.RefreshTokenRequest;
+import com.secure.jwttoken.payload.response.RefreshTokenResponse;
 import com.secure.jwttoken.repository.RefreshTokenRepository;
 import com.secure.jwttoken.repository.UserRepository;
+import com.secure.jwttoken.security.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,6 +27,7 @@ public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
 
+    private final JwtUtils jwtUtils;
     private final UserRepository userRepository;
 
     public Optional<RefreshToken> findByToken(String token){
@@ -44,7 +48,15 @@ public class RefreshTokenService {
         return refreshToken;
     }
 
+    public RefreshTokenResponse getRefreshTokenResponse(RefreshTokenRequest request){
+        String refreshToken = request.getRefreshToken();
+        return findByToken(refreshToken)
+                .map(this::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String token = jwtUtils.generateToken(user);
+                    return new RefreshTokenResponse(token,refreshToken,"Bearer");
+                }).orElseThrow(() -> new RefreshTokenException(refreshToken,"Refresh token is not in db"));
 
-
-
+    }
 }
