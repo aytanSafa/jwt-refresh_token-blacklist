42b586cec4ed765ad57ee6fa53aced798fbe3bc1
safaa <aytansafa@gmail.com>
2023-03-05 14:49:44 +0300

    Refresh Token entity created

---
+++ src/main/java/com/secure/jwttoken/entity/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.secure.jwttoken.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false,unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+
+}
