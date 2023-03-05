eb80f0559a6dc1e5e6de32a83b1e6bdc35e0acd5
safaa <aytansafa@gmail.com>
2023-03-05 14:51:09 +0300

    RefreshTokenRepository has created

---
+++ src/main/java/com/secure/jwttoken/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.secure.jwttoken.repository;
+
+import com.secure.jwttoken.entity.RefreshToken;
+import com.secure.jwttoken.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+    @Modifying
+    int deleteByUser(User user);
+}
