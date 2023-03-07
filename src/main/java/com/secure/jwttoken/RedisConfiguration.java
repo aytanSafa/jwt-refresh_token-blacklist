1ed0dbd2d4a81cdc27ad11f13a39a9dedbe37f0b
safaa <aytansafa@gmail.com>
2023-03-07 15:44:05 +0300

    RedisConfiguration class configured

---
+++ src/main/java/com/secure/jwttoken/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.secure.jwttoken;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost",6379);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+}
